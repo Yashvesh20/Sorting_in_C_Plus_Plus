ff5aeca9850ad64b5e21e3cd5b505ca9170f526e
Yashvesh20 <yashvesh20@gmail.com>
2025-09-30 20:05:49 +0530

    Create Selection_sort.cpp

---
+++ Selection_sort.cpp
@@ -0,0 +1,30 @@
+Yashvesh Singh 24070123138
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void selectionSort(std::vector<int>& arr){
+    int n = arr.size();
+    for(int i = 0; i < n - 1; i++){
+        int min_idx = i;
+        for(int j = i + 1; j < n; j++){
+            if(arr[j] < arr[min_idx]){
+                min_idx = j;
+            }
+        }
+        std::swap(arr[i], arr[min_idx]);
+    }
+}
+
+int main(){
+    std::vector<int> arr={220,7,20,22,1,5,8686};
+    int n=arr.size();
+    selectionSort(arr);
+    std::cout<<"Sorted array: ";
+    for(int i=0;i<n;i++)
+        std::cout<<arr[i]<<" ";
+    std::cout<<std::endl;
+    return 0;
+}
+output:
+Sorted array: 1 5 7 20 22 220 8686 
